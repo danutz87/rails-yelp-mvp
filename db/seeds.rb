c1431bcf09b7210ffb79624f9d7b4f8f8330b76c
daniel ghimpu <danielghimpu@yahoo.com>
2020-02-19 15:16:28 +0000

    Yelp mvp exercise done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+puts 'Creating 5 fake restaurants'
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  phone_number = Faker::PhoneNumber.cell_phone
+  category = CATEGORIES.sample
+
+  restaurant = Restaurant.new(name: name, address: address, phone_number: phone_number, category: category)
+  restaurant.save!
+end
+
+puts 'Done creating 5 fake restaurants'
